26b17b4325db4a0aaaeca9bd4ef9da86c3a293d3
crazywhittles <robbiejoe14@gmail.com>
2023-03-04 14:41:38 +0000

    people page

--- src/pages/people/people.js
+++ src/pages/people/people.js
@@ -6,7 +6,154 @@ function people() {
         <div className='people-body'>
             <Header />
             <div>
-                hi
+                <div className='people-title'>
+                    <p className='people-title-text'>
+                        My best
+                    </p>
+                    <p className='people-title-text2'>
+                        Friends
+                    </p>
+                    <p className='people-title-text'>
+                        forever and
+                    </p>
+                    <p className='people-title-text2'>
+                        ever (jk)
+                    </p>
+                </div>
+                <div className='people-container'>
+                    <div className='people-table'>
+                        <div class="card">
+                            <div class="card-border-top">
+                            </div>
+                            <div class="img">
+                                <img
+                                    className='car-pics'
+                                    src="https://cdn.discordapp.com/attachments/392231482875445258/1081312472671191081/ethic_2.jpg"
+                                    alt="cover">
+                                </img>
+                            </div>
+                            <span>
+                                Ethics
+                            </span>
+                            <p class="job">
+                                Full time Failure (jk)
+                            </p>
+                            <button>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                    <div className='people-table'>
+                        <div class="card card2">
+                            <div class="card-border-top">
+                            </div>
+                            <div class="img">
+                                <img
+                                    className='car-pics'
+                                    src="https://cdn.discordapp.com/attachments/810186625585774593/1081586923820290060/image.png"
+                                    alt="cover">
+                                </img>
+                            </div>
+                            <span>
+                                Ty...
+                            </span>
+                            <p class="job">
+                                Dropshipper Millionaire
+                            </p>
+                            <button>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                    <div className='people-table'>
+                        <div class="card card3">
+                            <div class="card-border-top">
+                            </div>
+                            <div class="img">
+                                <img
+                                    className='car-pics'
+                                    src="https://cdn.discordapp.com/attachments/392231482875445258/1081312472671191081/ethic_2.jpg"
+                                    alt="cover">
+                                </img>
+                            </div>
+                            <span>
+                                Ethics
+                            </span>
+                            <p class="job">
+                                Full time Failure (jk)
+                            </p>
+                            <button>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                    <div className='people-table'>
+                        <div class="card card4">
+                            <div class="card-border-top">
+                            </div>
+                            <div class="img">
+                                <img
+                                    className='car-pics'
+                                    src="https://cdn.discordapp.com/attachments/392231482875445258/1081312472671191081/ethic_2.jpg"
+                                    alt="cover">
+                                </img>
+                            </div>
+                            <span>
+                                Ethics
+                            </span>
+                            <p class="job">
+                                Full time Failure (jk)
+                            </p>
+                            <button>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                    <div className='people-table'>
+                        <div class="card card5">
+                            <div class="card-border-top">
+                            </div>
+                            <div class="img">
+                                <img
+                                    className='car-pics'
+                                    src="https://cdn.discordapp.com/attachments/392231482875445258/1081312472671191081/ethic_2.jpg"
+                                    alt="cover">
+                                </img>
+                            </div>
+                            <span>
+                                Ethics
+                            </span>
+                            <p class="job">
+                                Full time Failure (jk)
+                            </p>
+                            <button>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                    <div className='people-table'>
+                        <div class="card card6">
+                            <div class="card-border-top">
+                            </div>
+                            <div class="img">
+                                <img
+                                    className='car-pics'
+                                    src="https://cdn.discordapp.com/attachments/392231482875445258/1081312472671191081/ethic_2.jpg"
+                                    alt="cover">
+                                </img>
+                            </div>
+                            <span>
+                                Ethics
+                            </span>
+                            <p class="job">
+                                Full time Failure (jk)
+                            </p>
+                            <button>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
